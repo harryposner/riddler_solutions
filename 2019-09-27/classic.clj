08159d907ab62a00582a7e99033288b6546bf32b
Harry Posner <harryposner@users.noreply.github.com>
2019-09-27 19:59:39 -0400

    Reorder so solution actually runs with lein exec

--- 2019-09-27/classic.clj
+++ 2019-09-27/classic.clj
@@ -1,3 +1,15 @@
+(defn advance-bases [bases-per-hit base-runners]
+  (for [base (range (count base-runners))
+        :let [from-base (- base bases-per-hit)] ]
+       (cond (= from-base -1) true  ;;; batter
+             (< from-base -1) false
+             :else (nth base-runners from-base))))
+
+(defn n-runs [old-bases new-bases]
+  (let [n-runners (inc (count (filter true? old-bases)))
+        on-base (count (filter true? new-bases))]
+    (- n-runners on-base)))
+
 (defn half-inning [strikeout-rate bases-per-hit]
   (loop [runs 0
          outs 0
@@ -12,32 +24,18 @@
                     new-runs (n-runs base-runners new-bases)]
                 (recur new-runs outs new-bases))))))
 
-(defn advance-bases [bases-per-hit base-runners]
-  (for [base (range (count base-runners))
-        :let [from-base (- base bases-per-hit)] ]
-       (cond (= from-base -1) true  ;;; batter
-             (< from-base -1) false
-             :else (nth base-runners from-base))))
-
-(defn n-runs [old-bases new-bases]
-  (let [n-runners (inc (count (filter true? old-bases)))
-        on-base (count (filter true? new-bases))]
-    (- n-runners on-base)))
-
-
 (defn game [[home-strikeout-rate home-bases-per-hit]
             [away-strikeout-rate away-bases-per-hit]]
   "True if home team wins else false"
   (loop [home-score 0
          away-score 0
          inning-number 1]
-        ;;; Yes, this can play an unnecessary inning.  Whatever.
+        ;;; Yes, this can play an unnecessary half-inning.  Whatever.
         (let [new-home-score (+ home-score (half-inning home-strikeout-rate home-bases-per-hit))
               new-away-score (+ away-score (half-inning away-strikeout-rate away-bases-per-hit))]
           (if (and (>= inning-number 9) (not= new-home-score new-away-score))
               (> new-home-score new-away-score)
               (recur new-home-score new-away-score (inc inning-number))))))
-
 
 (defn season [games-per-matchup]
   (let [moonwalkers [6/10 1]
